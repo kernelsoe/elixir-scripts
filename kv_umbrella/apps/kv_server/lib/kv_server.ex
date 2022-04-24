1dd85674272f479a89ebf0231f1264c960aee37c
Kernel Soe <kernelsoe@gmail.com>
2022-04-24 16:10:01 +0900

    refactor

--- kv_umbrella/apps/kv_server/lib/kv_server.ex
+++ kv_umbrella/apps/kv_server/lib/kv_server.ex
@@ -26,25 +26,31 @@ defmodule KVServer do
 
   defp serve(socket) do
     msg =
-      case read_line(socket) do
-        {:ok, data} ->
-          case KVServer.Command.parse(data) do
-            {:ok, command} -> KVServer.Command.run(command)
-            {:error, _} = err -> err
-          end
+      with {:ok, data} <- read_line(socket),
+           {:ok, command} <- KVServer.Command.parse(data),
+           do: KVServer.Command.run(command)
 
-        {:error, _} = err ->
-          err
-      end
+    # msg =
+    #   case read_line(socket) do
+    #     {:ok, data} ->
+    #       case KVServer.Command.parse(data) do
+    #         {:ok, command} ->
+    #           KVServer.Command.run(command)
+
+    #         {:error, _} = err ->
+    #           err
+    #       end
+
+    #     {:error, _} = err ->
+    #       err
+    #   end
 
     write_line(socket, msg)
-
     serve(socket)
   end
 
   defp read_line(socket) do
-    {:ok, data} = :gen_tcp.recv(socket, 0)
-    data
+    :gen_tcp.recv(socket, 0)
   end
 
   defp write_line(socket, {:ok, text}) do
@@ -52,6 +58,7 @@ defmodule KVServer do
   end
 
   defp write_line(socket, {:error, :unknown_command}) do
+    # Known error; write to the client
     :gen_tcp.send(socket, "UNKNOWN COMMAND\r\n")
   end
 
@@ -61,6 +68,7 @@ defmodule KVServer do
   end
 
   defp write_line(socket, {:error, error}) do
+    # Unknown error; write to the client and exit
     :gen_tcp.send(socket, "ERROR\r\n")
     exit(error)
   end
